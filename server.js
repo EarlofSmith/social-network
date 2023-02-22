4991532444ff0452f47ddbce605b4431bbae11d4
EarlofSmith <r1smith3333@gmail.com>
2023-02-22 16:20:52 -0700

    added a server.js page

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const db = require('./config/connection');
+const routes = require('./routes');
+const mongoose = require('mongoose');
+
+const cwd = process.cwd();
+
+const PORT = process.env.port || 3001;
+const app = express();
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+// Log mongoose queries
+mongoose.set('debug', true);
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server for ${activity} running on port ${PORT}!`);
+  });
+});
